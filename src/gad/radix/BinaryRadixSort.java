2160f81ba255d5f716a97d460082677120c10cfc
schultzm <schultzmoritz@gmail.com>
2023-06-22 16:02:06 +0200

    kSort

--- src/gad/radix/BinaryRadixSort.java
+++ src/gad/radix/BinaryRadixSort.java
@@ -15,17 +15,14 @@ public final class BinaryRadixSort {
     public static void kSort(BinaryBucket from, BinaryBucket to, int binPlace) {
         for (int i = 0; i < from.getSize(); i++) {
             int value = from.getValue(i);
-            int bit = key(value, binPlace);
+            int temp = value;
+            int bit = key(temp, binPlace);
             if (bit == 1)
                 to.insertRight(value);
             else {
                 to.insertLeft(value);
             }
         }
-        BinaryBucket temp = from;
-        from = to;
-        to = temp;
-
     }
 
     public static void lastSort(BinaryBucket from, int[] to) {
@@ -36,11 +33,24 @@ public final class BinaryRadixSort {
     }
 
     public static void main(String[] args) {
-        int[] test = new int[10_000_000];
+        //int[] test = new int[10_000_000];
+        int[] test = new int[10];
         Random random = new Random();
+
         for (int i = 0; i < test.length; i++) {
-            test[i] = random.nextInt(Integer.MAX_VALUE);
+            test[i] = random.nextInt(100);
         }
+
+        BinaryBucket from = new BinaryBucket(10);
+        BinaryBucket to = new BinaryBucket(10);
+        for (int i = 0; i < test.length; i++) {
+            from.insertLeft(test[i]);
+        }
+
+        kSort(from, to, 1);
+        System.out.println(Arrays.toString(from.getBucket()));
+        System.out.println(Arrays.toString(to.getBucket()));
+
         int[] testTwo = Arrays.copyOf(test, test.length);
 
         long start = System.nanoTime();
